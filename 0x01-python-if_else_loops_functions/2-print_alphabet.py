c0338deb59f1e05063979ce35bc7ac597ebb2890
--global <--global>
2023-10-31 15:33:48 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-for i in "abcdfghijklmnoprstuvwxyz":
-    print(i, end=" ")
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    last_number = ((number * -1) % 10) * -1
+else:
+    last_number = number % 10
+if last_number > 5:
+    print(f"Last digit of {number:d} is {last_number:d} and is greater than 5")
+elif last_number == 0:
+    print(f"Last digit of {number:d} is {last_number:d} and is 0")
+else:
+    print(
+        f"Last digit of {number:d} is {last_number:d} and is less than 6 and not 0")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in "abcdfghijklmnoprstuvwxyz":
+    if i != "q" and i != "e":
+        print(i, end=" ")
+    continue
