d62d5ec10e4585e2f0175462aab7dba332cc6505
--global <--global>
2023-10-31 15:50:45 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -6,9 +6,9 @@ if number < 0:
 else:
     last_number = number % 10
 if last_number > 5:
-    print(f"Last digit of {number:d} is {last_number:d} and is greater than 5")
+    print("Last digit of {} is {} and is greater than 5".format(number, last_number))
 elif last_number == 0:
-    print(f"Last digit of {number:d} is {last_number:d} and is 0")
+    print("Last digit of {} is {} and is 0".format(number ,last_number))
 else:
     print(
-        f"Last digit of {number:d} is {last_number:d} and is less than 6 and not 0")
+        "Last digit of {} is {} and is less than 6 and not 0".format(number, last_number))
