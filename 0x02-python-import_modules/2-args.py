e6f711a71d438a1eb377c538b8f210111cee0211
--global <--global>
2023-11-02 16:34:19 -0700

    last chance

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     length = len(sys.argv) - 1
-    if length != 0:
+    if length > 0:
         if length == 1:
             print("{} argument:".format(length))
             print("{}: {}".format(length, sys.argv[length]))
