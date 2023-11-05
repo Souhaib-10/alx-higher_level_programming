a22dd65cafebdf5e65f957a9dc6743261354017d
--global <--global>
2023-11-05 05:48:14 -0800

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if isinstance(my_list, list):
-        length = len(my_list)-1
-        copy_list = my_list.copy()
-        if idx > length or idx < 0:
-            return None
-        else:
-            copy_list[idx] = element
-            return copy_list
+    length = len(my_list)-1
+    copy_list = my_list.copy()
+    if idx > length or idx < 0:
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list
