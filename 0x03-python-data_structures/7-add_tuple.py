8b53afb258f404d12b9f8d5d2e9a9d81796d10f1
--global <--global>
2023-11-06 08:24:09 -0800

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,15 +3,16 @@ def add_tuple(tuple_a=(), tuple_b=()):
     result_tuple = ()
     if len(tuple_b) < 2:
         if len(tuple_b) < 1:
-            return (tuple_a)
+            tuple_b = (0, 0)
         else:
-            tuple_b += (0,)
-            for i, j in zip(tuple_a, tuple_b):
-                result_tuple += ((i+j),)
-            return result_tuple
-    else:
-        first_two_elements = tuple_a[:2]
-        first_two_elements_b = tuple_b[:2]
-        for i, j in zip(first_two_elements, first_two_elements_b):
-            result_tuple += ((i+j),)
-        return result_tuple
+            tuple_b = (tuple_b[0], 0)
+    if len(tuple_a) < 2:
+        if len(tuple_a) < 1:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    first_two_elements = tuple_a[:2]
+    first_two_elements_b = tuple_b[:2]
+    for i, j in zip(first_two_elements, first_two_elements_b):
+        result_tuple += ((i+j), )
+    return result_tuple
