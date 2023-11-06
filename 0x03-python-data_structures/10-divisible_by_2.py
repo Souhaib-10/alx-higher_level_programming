ecd4d441807fbf34578a40871669073d6c0121b6
--global <--global>
2023-11-06 08:42:26 -0800

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+def divisible_by_2(my_list=[]):
+    bool_list = []
+    for i in my_list:
+        if i % 2 == 0:
+            bool_list.append(True)
+        else:
+            bool_list.append(False)
+    return bool_list
