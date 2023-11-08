cdee607b55429e3b4a0c36cdef83258b40cb29f3
--global <--global>
2023-11-08 14:37:39 -0800

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary != None:
-        values_dict = a_dictionary.values()
-        max_values = max(values_dict)
-        return max_values        
-    return None
+    if not a_dictionary:
+        return None
+    return (max(a_dictionary, key=a_dictionary.get))
