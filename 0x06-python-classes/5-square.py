378b967c2e5adf4777d7394f3dd6b93ca337b156
--global <--global>
2023-11-21 15:02:25 -0800

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+''' square validation class'''
+
+
+class Square:
+
+    '''initial object from square class
+
+
+    Args:
+        size (int): size squarep
+    '''
+
+    def __init__(self, size):
+        ''' change size value '''
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''Return the current area the square.'''
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        '''Return current value size'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        ''' change size value '''
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#"*self.__size)
+            print("")
