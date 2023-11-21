e921bed3c23c2d18d1909a84ed67532b1fdcb2fb
--global <--global>
2023-11-21 14:23:26 -0800

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' square validation class'''
+
+
+class Square:
+
+    '''initial object from square class
+
+
+    Args:
+        size (int): size square
+    '''
+
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+
+        '''Return the current area the square.'''
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+
+        '''Return current value size'''
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
