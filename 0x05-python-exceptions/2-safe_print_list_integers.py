0417c3766de6782975314a4d21b350199e26e7e0
--global <--global>
2023-11-22 12:43:54 -0800

    Print and count integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except IndexError:
+            raise
+            print("list index out of range")
+        except (ValueError, TypeError):
+            continue
+
+    print("")
+    return count
