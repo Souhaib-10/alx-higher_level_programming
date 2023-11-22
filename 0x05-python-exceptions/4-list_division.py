33ef1aaa7f6a8a53caa36f6af799fdede5e63af8
--global <--global>
2023-11-22 13:10:51 -0800

    task N4 and N5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
-        div = 0
+        list_div = 0
         try:
-            div = my_list_1[i] / my_list_2[i]
+            list_div = my_list_1[i] / my_list_2[i]
         except (TypeError, ValueError):
             print("wrong type")
         except (ZeroDivisionError, ValueError):
@@ -12,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except (IndexError, ValueError):
             print("out of range")
         finally:
-            result.append(div)
+            result.append(list_div)
     return result
