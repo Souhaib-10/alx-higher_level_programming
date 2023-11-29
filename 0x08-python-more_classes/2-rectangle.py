62c909ba345c706b164cee841257f6ec11084f7c
--global <--global>
2023-11-29 12:27:29 -0800

    Area and Perimeter mandatoryé

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -57,6 +57,6 @@ class Rectangle:
 
     def perimeter(self):
         '''calculate perimeter of the triangle'''
-        if self.__width == 0 or self.__width == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
