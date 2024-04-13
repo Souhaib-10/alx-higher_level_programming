52d43947ecefb069df04fba3565a3c0bb029982f
souhaib10 <souhaibb1998@gmail.com>
2024-04-12 17:30:22 -0700

    first test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -6,13 +6,30 @@ have just one function.
 
 
 def add_integer(a, b=98):
-    """ return addition of two numbers"""
+    """ return addition of two numbers
+
+    Args:
+        a: the fierst argument (int, flaot)
+        b: the second argument (int, float)
+
+    Raises:
+        TypeError : if a, b are not int or float
+
+    Returns:
+        the sum of two arguments
+    """
+
     if type(a) not in [int, float]:
-        raise typeError("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
-        raise typeError("b must be an integer")
+        raise TypeError("b must be an integer")
     if type(a) is float:
         a = int(a)
     if type(b) is float:
         b = int(b)
     return a+b
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
