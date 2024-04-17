6eb96e4a6f5a8baffb6526760a116f5ce66df67e
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 07:07:51 -0700

    class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' script to create class have method area '''
+
+
+class BaseGeometry:
+    ''' define class BaseGeometry'''
+
+
+def area(self):
+    '''
+        Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: If the area calculation method is not implemented.
+
+        Returns:
+            str: A message Exception
+    '''
+    return raise Exception('area() is not implemented')
