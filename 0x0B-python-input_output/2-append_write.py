f3734f38b128363ff93760d19b831ecf156757c0
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 15:46:26 -0700

    add to file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''script for function append text end of file'''
+
+
+def append_write(filename="", text=""):
+    '''Append a string to text file
+
+    Args:
+    filename (str): the name of the file
+    text (str): the text to append to the file
+    Returns:
+        return  the number of characters added
+    '''
+
+    with open(filename="", "a") as f:
+        return f.write(text)
