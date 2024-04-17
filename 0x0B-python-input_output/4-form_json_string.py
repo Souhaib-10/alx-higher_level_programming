8c71cd579705b0bda1c2cb7f14dfff6a97c26d5f
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 16:06:22 -0700

    json to obj

---
+++ 0x0B-python-input_output/4-form_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script function change json to string"""
+import json
+
+
+def from_json_string(my_str):
+    ''' function change json to obj
+
+    Args:
+        my_str: json data
+
+    Returns:
+        return obj data
+    '''
+
+    return json.loads(my_obj)
