d08bf1a54a36d43a061a50cc571bca5846b1d100
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 15:25:11 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-''' script for read file
-
-    Args:
-    filename (str): name of file
-'''
-
 def read_file(filename=""):
     """function that reads a text file and prints it"""
 
