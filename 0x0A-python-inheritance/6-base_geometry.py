ff8825a1314a951ac8eb4a9348e59f26dc86caf3
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 07:12:24 -0700

    class BaseGeometry

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -4,16 +4,14 @@
 
 class BaseGeometry:
     ''' define class BaseGeometry'''
+    def area(self):
+        '''
+            Calculate the area of the geometric shape.
 
+            Raises:
+                Exception: If the area calculation method is not implemented.
 
-def area(self):
-    '''
-        Calculate the area of the geometric shape.
-
-        Raises:
-            Exception: If the area calculation method is not implemented.
-
-        Returns:
-            str: A message Exception
-    '''
-    return raise Exception('area() is not implemented')
+            Returns:
+                str: A message Exception
+        '''
+        raise Exception("area() is not implemented")
