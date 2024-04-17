0312e18975bd5f8db53d52206efa0f77714f4996
souhaib10 <souhaibb1998@gmail.com>
2024-04-17 15:33:36 -0700

    write in file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,14 @@
 
 
 def write_file(filename="", text=""):
+    ''' function to write in file
+
+    Args:
+        filename (str): name file
+        text (str): text to write in file
+    Return :
+        text written in file
+    '''
+
     with open(filename, "w", encoding="UFT-8") as f:
         return f.write(text)
