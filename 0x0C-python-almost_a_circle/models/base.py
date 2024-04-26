ab3945604c842f7ac733fec62f5dd0262bfc62bb
root <souhaibb1998@gmail.com>
2024-04-26 11:02:08 -0700

    Add to_json_string method in class base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,3 +17,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' Return the json serialization of list
+        Args:
+            list_dictionaries (list): A list of dict
+        '''
+        if list_dictionaries is None:
+            return []
+        else:
+            return json.dumps(list_dictionaries)
