aab38d9e6134128e54e873a5a33fa6671298157f
souhaib10 <souhaibb1998@gmail.com>
2024-04-30 10:53:33 -0700

    task 19 correct

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+''' define function copy_list'''
+
+
 def copy_list(l):
+    ''' script to create function for copy list
+    Args:
+        l (list): the list to copied
+    '''
     return l.copy()
