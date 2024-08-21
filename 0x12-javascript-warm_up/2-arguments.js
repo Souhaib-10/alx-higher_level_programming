7dc35a2162062564ded2d295f17683186e8352d5
souhaib-10 <souhaibb1998@gmail.com>
2024-08-21 11:22:43 -0700

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const countArg = process.argv.length;
-if (countArg > 2) {
+if (countArg === 2) {
+  console.log('Argument found');
+} else if (countArg === 3) {
   console.log('Arguments found');
 } else {
   console.log('No argument');
