8edbc986f2cc48e68eb3703bbb38e1ecebf8c57c
souhaib-10 <souhaibb1998@gmail.com>
2024-08-22 11:59:22 -0700

    Factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-const arg = parseInt(process.argv[2]);
-let result = 1;
-if (isNaN(arg)) {
-  console.log(1);
-} else {
-  for (let i = arg; i > 0; i--) {
-    result *= i;
+function factoriel (arg) {
+  let result = 1;
+  if (isNaN(arg)) {
+    console.log(1);
+  } else {
+    for (let i = arg; i > 0; i--) {
+      result *= i;
+    }
+    console.log(result);
   }
-  console.log(result);
 }
+const argp = parseInt(process.argv[2]);
+factoriel(argp);
