d213a6194ada2d363e87e2c1bb3d7c2b2d244158
souhaib-10 <souhaibb1998@gmail.com>
2024-08-22 11:45:15 -0700

    Add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-	console.log(a + b);
+function add (a, b) {
+  console.log(a + b);
 }
 const arg1 = parseInt(process.argv[2]);
 const arg2 = parseInt(process.argv[3]);
