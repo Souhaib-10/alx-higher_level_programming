c071639cf02f7cdad1d67157b3c49bb0817f8662
souhaib-10 <souhaibb1998@gmail.com>
2024-08-22 11:40:13 -0700

    Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const arg = process.argv[2];
 const parseArg = parseInt(arg);
-if ( isNaN(parseArg)){
-	console.log('Missing size');
-}else {
-	for (let i = 0; i < parseArg; i++){
-		let numberx = '';
-		for ( let j = 0; j < parseArg; j++) {
-			numberx += 'X';
-		}
-		console.log(numberx);
-	}
+if (isNaN(parseArg)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseArg; i++) {
+    let numberx = '';
+    for (let j = 0; j < parseArg; j++) {
+      numberx += 'X';
+    }
+    console.log(numberx);
+  }
 }
-
