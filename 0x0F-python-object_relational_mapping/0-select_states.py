a72a457d109598f74c6141e2583609460cc1ab6f
souhaib-10 <souhaibb1998@gmail.com>
2024-08-27 10:48:35 -0700

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+''' use ORM to connect a database with python code
+    lists all states from the database hbtn_0e_0_usa
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db =  MySQLdb.connect(user=sys.argv[1],password=sys.argv[2],database=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
