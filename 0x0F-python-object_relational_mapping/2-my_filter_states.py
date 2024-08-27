45ad11bbae6fe22e4c4393d6184c5d56dec9e99d
souhaib-10 <souhaibb1998@gmail.com>
2024-08-27 11:40:28 -0700

    Filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' use ORM to connect a database with python code
+    lists all states with a name starting with N
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    argu = sys.argv
+    db = MySQLdb.connect(user=argu[1], password=argu[2], database=argu[3])
+    c = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id"
+    query = query.format(argu[4])
+    c.execute(query)
+    for state in c.fetchall():
+        print(state)
+    c.close()
+    db.close()
