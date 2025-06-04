63e8b003bbde3d6db7ef3ccf8d46125b68c399ab
souhaib-10 <souhaibb1998@gmail.com>
2025-06-04 07:36:32 -0700

    use get function to get the value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
     url = sys.argv[1]
     request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        print(response.headers["X-Request-Id"])
+        print(dict(response.headers).get("X-Request-Id"))
