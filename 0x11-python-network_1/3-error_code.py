dca333b5e9b360b8f226230b09047ffbfaf53bb8
souhaib-10 <souhaibb1998@gmail.com>
2025-06-08 11:08:37 -0700

    fix problem tasks 3 and 4

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 
     req = Request(sys.argv[1])
     try:
-        with urlopen(req) as reponse:
-            print(reponse.read().decode("ascii"))
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
     except HTTPError as e:
-        print('Error code: ', e.code)
+        if hasattr(e, 'code'):
+            print('Error code: ', e.code)
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     req = requests.get('https://alx-intranet.hbtn.io/status')
 
-    print('Body response:$')
-    print("\t- type: {}$".format(type(req.text)))
-    print("\t- content: {}$".format(req.text))
+    print('Body response:')
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
