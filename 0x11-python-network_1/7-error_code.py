07520389bca0eba6d39f5007f6055a263375de6f
souhaib-10 <souhaibb1998@gmail.com>
2025-06-08 11:44:17 -0700

    7. Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Python script that takes in a URL,
+    sends a request to the URL and displays the body of the response
+    display errors
+'''
+
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+    import sys
+
+    req = Request(sys.argv[1])
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    except HTTPError as e:
+        if e.code >= 400:
+            print("Error code: {}".format(e.code))
