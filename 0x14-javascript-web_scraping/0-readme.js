7eb2db003bedcb7df40d7ec276e972ecddf506b2
souhaib-10 <souhaibb1998@gmail.com>
2025-06-13 07:48:33 -0700

    fix empty file logic for task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,5 +5,8 @@ const process = require('process');
 const path = process.argv[2];
 
 fs.readFile(path, 'utf8', (err, data) => {
-  console.log(data || err);
+  if (err){
+	  console.log(err);
+  }
+  console.log(data);
 });
