ea5764f9aa592d69a71ee4d615cc4ef72bec2a0c
souhaib-10 <souhaibb1998@gmail.com>
2025-06-17 08:11:35 -0700

    task 6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-const request = require("request");
-const process = require("process");
+const request = require('request');
+const process = require('process');
 
 const url = process.argv[2];
 request(url, (error, response, body) => {
-  if (error){
-	  console.log(error.message);
+  if (error) {
+    console.log(error.message);
   }
-  let data = JSON.parse(body);
-  let numberValidate = data.reduce((acc, dt) => {
-     if (dt.completed === true) {
-        acc[dt.userId] = (acc[dt.userId] || 0) + 1
-     }
-     return acc
-  },{});
+  const data = JSON.parse(body);
+  const numberValidate = data.reduce((acc, dt) => {
+    if (dt.completed === true) {
+      acc[dt.userId] = (acc[dt.userId] || 0) + 1;
+    }
+    return acc;
+  }, {});
   console.log(numberValidate);
-})
+});
